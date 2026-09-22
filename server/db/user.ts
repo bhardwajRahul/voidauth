8ebbff182ec0d3e8c14c011cc2b59ae79a21e218
notquitenothing <nqn@notquitenothing.com>
2026-09-21 19:44:14 -0500

    Added backchannel logout support. Admin user signout and delete now also ends client user sessions when possible using backchannel logout. When a user deletes their account, attempt to end their client sessions also using backchannel logout.

--- server/db/user.ts
+++ server/db/user.ts
@@ -7,7 +7,6 @@ import { ADMIN_USER, ADMIN_GROUP, TTLs } from '@shared/constants'
 import { randomBytes, randomUUID } from 'crypto'
 import type { Flag } from '@shared/db/Flag'
 import appConfig from '../util/config'
-import type { OIDCPayload } from '@shared/db/OIDCPayload'
 import { hasTOTP } from './totp'
 import { getUserPasskeys } from './passkey'
 import { argon2 } from '../util/argon2id'
@@ -183,10 +182,6 @@ export async function getUserByUsername(username: string): Promise<UserDetails |
 export async function checkPasswordHash(userId: string, password: string): Promise<boolean> {
   const user = await db().select().table<User>(TABLES.USER).where({ id: userId }).first()
   return !!user && !!password && !!user.passwordHash && argon2.verify(user.passwordHash, password)
-}
-
-export async function endSessions(userId: string) {
-  await db().table<OIDCPayload>(TABLES.OIDC_PAYLOADS).delete().where({ type: 'Session', accountId: userId })
 }
 
 export async function findAccount(_: KoaContextWithOIDC | null, id: string): Promise<Account | undefined> {
